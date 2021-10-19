70217c9837959554d11fcfa04108305d9cb7fd2f
Debbiche-Senda <debbiche.senda.douda@gmail.com>
2021-10-19 16:16:53 +0100

    first-appload-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import imageInSrc from "./imageInSrc.jpg";
+import "./Image.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="seediv" style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="titlered"> SEE</h1>
+        <br />
+        <img className="myimg1" src="./imageInPublic.jpg" alt="img"/> <br />
+        <img className="myimg2" src={imageInSrc} alt="img" /> 
+      </div>
+      <video width="320" height="240" controls>
+        {" "}
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
